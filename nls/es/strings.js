0c3e3d05e597d6cfd6dda9260eb49fb8285ec751
Chema <jbalsas@gmail.com>
2012-10-06 23:44:48 +0200

    Add localization strings for testing

---
+++ nls/es/strings.js
@@ -0,0 +1,2 @@
+"TEST_UNUSED"       : "The key is not used anymore"
+"TEST_UNTRANSLATED" : "The key is not translated"
---
+++ nls/root/strings.js
@@ -0,0 +1,2 @@
+"TEST_MISSING"      : "The key is missing from the locale"
+"TEST_UNTRANSLATED" : "The key is not translated"
