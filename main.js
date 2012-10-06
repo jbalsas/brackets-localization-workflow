afc0ba637d8e89bb8aca46f6540def99617d7c21
Chema <jbalsas@gmail.com>
2012-10-06 12:42:09 +0200

    Added jump to line feature

--- main.js
+++ main.js
@@ -10,13 +10,15 @@ define(function (require, exports, module) {
         DocumentManager         = brackets.getModule("document/DocumentManager"),
         Menus                   = brackets.getModule("command/Menus"),
         ExtensionUtils          = brackets.getModule("utils/ExtensionUtils"),
+        StringUtils             = brackets.getModule("utils/StringUtils"),
         FileUtils               = brackets.getModule("file/FileUtils"),
+        StatusBar               = brackets.getModule("widgets/StatusBar"),
         NativeFileSystem        = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
         strings                 = brackets.getModule("i18n!nls/strings");
     
     var SHOW_LOCALIZATION_STATUS = "localizationWorkflow.show";
 
-    var lineRegExp      = new RegExp('[^\r\n]+', 'g'),
+    var lineRegExp      = new RegExp('[^\r\n\f]+', 'g'),
         entryKeyRegExp  = new RegExp('^(\\s*)"([^"]*)'),
         entryDescRegExp = new RegExp('"([^"]*)([^:]*):\\s"([^"]*)');
     
@@ -32,12 +34,19 @@ define(function (require, exports, module) {
     function _parseStrings(text) {
         var data, strings = {};
         
-        text.match(lineRegExp).forEach(function (line, index) {
-            if (entryKeyRegExp.test(line)) {
-                data = entryDescRegExp.exec(line);
-                strings[data[1]] = {line: index, desc: data[3]};
+        var lines = StringUtils.getLines(text);
+        var match;
+        
+        while ((match = lineRegExp.exec(text)) !== null) {
+            if (entryKeyRegExp.test(match)) {
+                var lineNum = StringUtils.offsetToLineNum(lines, match.index);
+                
+                data = entryDescRegExp.exec(match);
+                strings[data[1]] = {desc: data[3],
+                                    start: {line: lineNum, ch: 0},
+                                    end: {line: lineNum, ch: match[0].length}};
             }
-        });
+        }
         
         return strings;
     }
@@ -45,15 +54,29 @@ define(function (require, exports, module) {
     function _compareLocales() {
         
         var key, $row;
+        
+        var _clickHandler = function (locale, entry) {
+            return function () {
+                StatusBar.showBusyIndicator(true);
+                CommandManager.execute(Commands.FILE_OPEN, {fullPath: stringsPath + "/" + locale + "/strings.js"})
+                    .done(function (doc) {
+                        EditorManager.getCurrentFullEditor().setSelection(entry.start, entry.end);
+                        StatusBar.hideBusyIndicator();
+                    });
+            };
+        };
+        
         for (key in rootStrings) {
             if (rootStrings.hasOwnProperty(key)) {
                 if (localeStrings[key] === undefined) {
                     $row = $("<tr>").append($("<td>").html(key)).append($("<td>").html("The key is missing")).addClass("missing");
                     $localizationResults.append($row);
+                    $row.on("click", _clickHandler("root", rootStrings[key]));
                 } else {
                     if (localeStrings[key].desc === rootStrings[key].desc) {
                         $row = $("<tr>").append($("<td>").html(key)).append($("<td>").html("The key is not translated")).addClass("untranslated");
                         $localizationResults.append($row);
+                        $row.on("click", _clickHandler($localeSelector.val(), localeStrings[key]));
                     }
                     delete localeStrings[key];
                 }
@@ -64,11 +87,9 @@ define(function (require, exports, module) {
             if (localeStrings.hasOwnProperty(key)) {
                 $row = $("<tr>").append($("<td>").html(key)).append($("<td>").html("The key is not used anymore")).addClass("unused");
                 $localizationResults.append($row);
+                $row.on("click", _clickHandler($localeSelector.val(), localeStrings[key]));
             }
         }
-        
-        $localizationResults.find('tr').click(function () {
-        });
     }
     
     function _handleShowLocalizationStatus() {
